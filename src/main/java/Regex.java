6248725cb4c0f82e6330004fdc890d8f8994a6da
Ji <greenyjwk@gmail.com>
2022-04-07 23:25:28 -0400

    Connetion to MongoDB Atlas

--- src/main/java/Regex.java
+++ src/main/java/Regex.java
@@ -8,10 +8,9 @@ import com.mongodb.client.MongoCursor;
 import static com.mongodb.client.model.Filters.*;
 import org.bson.Document;
 import org.bson.conversions.Bson;
+import org.bson.types.ObjectId;
+import java.util.ArrayList;
 
-//Log message control imports
-import java.util.logging.Logger;
-import java.util.logging.Level;
 
 public class Regex{
     // Attribute used by Mongo in several methods
@@ -24,6 +23,7 @@ public class Regex{
 
 
     public static void main(String[] args) {
+
         Regex app = new Regex();
 
         // Make the connection
@@ -42,9 +42,10 @@ public class Regex{
         boolean connSuccess = false;
 
         // Connect to the Mongo Database
-        client = MongoClients.create("mongodb://localhost:27017");
+        client = MongoClients.create("mongodb+srv://Ji:1q2w3e4r5tzxcvb@iste-610-team2.bafxi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
+//        client = MongoClients.create("mongodb+srv://iste-610-team2.bafxi.mongodb.net/myFirstDatabase?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority");
 
-//        System.out.println("\n* Connection to server completed\n");
+        System.out.println("\n* Connection to server completed\n");
 
         try {
             // Use try/catch so that you will always close the database (finally)
@@ -53,14 +54,14 @@ public class Regex{
             dbList = client.listDatabaseNames();
             //Does Database Exist?
 
-            String dbName = "SampleSocial";
+            String dbName = "OlympicHistory";
             if (objectExists(dbList, dbName)) {  //returns true or false
-//                System.out.println("\n* Database found\n");
+                System.out.println("\n* Database found\n");
                 sampleDB = client.getDatabase(dbName); //connect to the database
-//                System.out.println("\n* Connection to database completed\n");
+                System.out.println("\n* Connection to database completed\n");
                 connSuccess = true;
             } else {  //db not found
-//                System.out.println("\nDATABASE NOT FOUND\n");
+                System.out.println("\nDATABASE NOT FOUND\n");
                 sampleDB = null; //DB not found
                 connSuccess = false;
                 client.close();
@@ -71,13 +72,20 @@ public class Regex{
             collList = sampleDB.listCollectionNames();
 
             //get the collection
-            String colName = "Tweets";
+            String colName = "Olympic";
             if (objectExists(collList, colName)) {  //returns true or false
-//                System.out.println("\n* Collection found\n");
+                System.out.println("\n* Collection found\n");
                 collection = sampleDB.getCollection(colName);
-//                System.out.println("\n* Collection connection completed\n");
+
+                // This is sampele query
+                Document query = new Document(  "_id", new ObjectId("623903bbadb4ce02b755122d") );
+                Document result = collection.find(query).iterator().next();
+
+                System.out.println("Query Result: " + result.getString("Name"));
+                System.out.println("\n* Collection connection completed\n");
+
             } else {  //collection not found
-//                System.out.println("\n* COLLECTION NOT FOUND\n");
+                System.out.println("\n* COLLECTION NOT FOUND\n");
                 collection = null;
                 connSuccess = false;
                 client.close();
@@ -89,7 +97,7 @@ public class Regex{
         return connSuccess;
     }//connections
 
-    private void mongoFind() {
+    private ArrayList<Document> mongoFind() {
         String searchText1 = "\\bdish\\b";
         String searchText2 = "\\bgood\\b";
         Bson query1 = regex("text", searchText1, "i");
@@ -97,15 +105,24 @@ public class Regex{
         Bson query3 = and(query1,query2);
         cursor = collection.find(query3).iterator();
 
+
+        ArrayList<Document> list = new ArrayList<>();
+
         //traverse cursor
         int cnt = 0;
         while(cursor.hasNext()) {
             Document doc = cursor.next();
-            System.out.println(doc.getString("text"));
+            System.out.println(doc.getString("Name"));
+            list.add(doc);
             cnt = cnt+1;
         }
+
+        System.out.println(list);
         System.out.println("\nThe count is " + cnt + "\n");
+        return list;
     }
+
+
 
     private boolean objectExists(MongoIterable <String> objectList, String objectName) {
         for (String name : objectList) {
